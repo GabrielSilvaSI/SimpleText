557a80c307bc2590c1b3fc8bf287b2445a5ef672
Gabriel da Silva Lima <90117166+GabrielSilvaSI@users.noreply.github.com>
2024-01-10 00:52:48 -0400

    Login Auth
    Login Auth

--- app/src/main/java/com/banilla/simplechat/ChatActivity.kt
+++ app/src/main/java/com/banilla/simplechat/ChatActivity.kt
@@ -2,10 +2,19 @@ package com.banilla.simplechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class ChatActivity : AppCompatActivity() {
+    lateinit var textViewMessages: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        textViewMessages = findViewById(R.id.textViewMessages)
+        val currentUser = intent.getStringExtra("user")
+        textViewMessages.text = currentUser.toString()
     }
 }
--- app/src/main/java/com/banilla/simplechat/MainActivity.kt
+++ app/src/main/java/com/banilla/simplechat/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.banilla.simplechat
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +12,6 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
@@ -31,17 +31,21 @@ class MainActivity : AppCompatActivity() {
         textViewStatus = findViewById(R.id.textViewStatus)
     }
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        startChat()
+    }
+
     fun signinAuth(view: View){
         auth.createUserWithEmailAndPassword(textInputEmail.text.toString(), textInputPassword.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    textViewStatus.text = "New user registered!"
-                    textViewStatus.isVisible = true
                     textInputEmail.setText("")
                     textInputPassword.setText("")
+                    startChat()
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
@@ -55,5 +59,44 @@ class MainActivity : AppCompatActivity() {
                     textInputPassword.setText("")
                 }
             }
+    }
+
+    fun loginAuth(view: View){
+        auth.signInWithEmailAndPassword(textInputEmail.text.toString(), textInputPassword.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    startChat()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    textViewStatus.text = "Failed to log in. Nonexistent account or wrong password!"
+                    textViewStatus.isVisible = true
+                    textInputPassword.setText("")
+                }
+            }
+    }
+
+    private fun startChat() {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, ChatActivity::class.java)
+            intent.putExtra("user", currentUser.toString())
+            startActivity(intent)
+            Toast.makeText(
+                baseContext,
+                "Login successful!",
+                Toast.LENGTH_SHORT,
+            ).show()
+            finish()
+
+        }
     }
 }
