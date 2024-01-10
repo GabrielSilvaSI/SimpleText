3048325a38d96f2ce177da7f454948ef655b8641
Gabriel da Silva Lima <90117166+GabrielSilvaSI@users.noreply.github.com>
2024-01-10 01:06:10 -0400

    Logout
    Logout

--- app/src/main/java/com/banilla/simplechat/ChatActivity.kt
+++ app/src/main/java/com/banilla/simplechat/ChatActivity.kt
@@ -1,20 +1,37 @@
 package com.banilla.simplechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
 class ChatActivity : AppCompatActivity() {
     lateinit var textViewMessages: TextView
+    lateinit var textViewUser: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        textViewMessages = findViewById(R.id.textViewMessages)
+        textViewUser = findViewById(R.id.textViewUser)
+
         val currentUser = intent.getStringExtra("user")
-        textViewMessages.text = currentUser.toString()
+        textViewUser.text = currentUser.toString().split("@")[1]
+    }
+
+    fun deauth(view: View){
+        Firebase.auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        Toast.makeText(
+            baseContext,
+            "Log out successful!",
+            Toast.LENGTH_SHORT,
+        ).show()
+        finish()
     }
 }
