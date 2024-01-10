a190c9be2f04252558e4d202bba2b05546e4936f
Gabriel da Silva Lima <90117166+GabrielSilvaSI@users.noreply.github.com>
2024-01-10 03:14:40 -0400

    Login update
    Login update

--- app/src/main/java/com/banilla/simplechat/MainActivity.kt
+++ app/src/main/java/com/banilla/simplechat/MainActivity.kt
@@ -4,21 +4,24 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.text.TextUtils
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.isVisible
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var dbRef: DatabaseReference
+    private lateinit var textInputUser: EditText
     private lateinit var textInputEmail: EditText
     private lateinit var textInputPassword: EditText
-    private lateinit var textViewStatus: TextView
+    private lateinit var textInputConfirmPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
+        textInputUser = findViewById(R.id.editTextUser)
         textInputEmail = findViewById(R.id.editTextEmail)
         textInputPassword = findViewById(R.id.editTextPassword)
-        textViewStatus = findViewById(R.id.textViewStatus)
+        textInputConfirmPassword = findViewById(R.id.editTextConfirmPassword)
     }
 
     public override fun onStart() {
@@ -37,74 +41,105 @@ class MainActivity : AppCompatActivity() {
         startChat()
     }
 
-    fun signinAuth(view: View){
-        val tempUser = textInputEmail.text.toString()
+    fun buttonSignin(view: View){
+        val tempUser = textInputUser.text.toString()
+        val tempEmail = textInputEmail.text.toString()
         val tempPassword = textInputPassword.text.toString()
-        if(tempUser!="" && tempPassword!=""){
-            auth.createUserWithEmailAndPassword(tempUser, tempPassword)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        textInputEmail.setText("")
-                        textInputPassword.setText("")
-                        startChat()
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            baseContext,
-                            "Authentication failed.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-                        textViewStatus.text = "The email account is already registered!"
-                        textViewStatus.isVisible = true
-                        textInputPassword.setText("")
-                    }
-                }
+        val tempConfirmPassword = textInputConfirmPassword.text.toString()
+        if(TextUtils.isEmpty(tempUser)){
+            Toast.makeText(applicationContext, "A username is required", Toast.LENGTH_SHORT).show()
+        }else
+        if(TextUtils.isEmpty(tempEmail)){
+            Toast.makeText(applicationContext, "A email is required", Toast.LENGTH_SHORT).show()
+        }else
+        if(TextUtils.isEmpty(tempPassword)){
+            Toast.makeText(applicationContext, "A password is required", Toast.LENGTH_SHORT).show()
+        }else
+        if(TextUtils.isEmpty(tempConfirmPassword)){
+            Toast.makeText(applicationContext, "A confirm password is required", Toast.LENGTH_SHORT).show()
+        }else
+        if(tempPassword != tempConfirmPassword){
+            Toast.makeText(applicationContext, "Passwords not match", Toast.LENGTH_SHORT).show()
+        }else{
+            signinAuth(tempUser, tempEmail, tempPassword)
+        }
+
+    }
+
+    fun buttonLogin(view: View){
+        val tempEmail = textInputEmail.text.toString()
+        val tempPassword = textInputPassword.text.toString()
+        if(TextUtils.isEmpty(tempEmail)){
+            Toast.makeText(applicationContext, "A email is required", Toast.LENGTH_SHORT).show()
+        }else if(TextUtils.isEmpty(tempPassword)){
+                Toast.makeText(applicationContext, "A password is required", Toast.LENGTH_SHORT).show()
+            }else{
+                loginAuth(tempEmail, tempPassword)
         }
     }
 
-    fun loginAuth(view: View){
-        val tempUser = textInputEmail.text.toString()
-        val tempPassword = textInputPassword.text.toString()
-        if(tempUser!="" && tempPassword!="") {
-            auth.signInWithEmailAndPassword(tempUser, tempPassword)
+    private fun signinAuth(tempUser: String,tempEmail: String,tempPassword: String){
+        auth.createUserWithEmailAndPassword(tempEmail, tempPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user: FirebaseUser? = auth.currentUser
+                    val userId: String = user!!.uid
+                    dbRef = FirebaseDatabase.getInstance().getReference("Users").child(userId)
+                    val hashMap: HashMap<String,String> = HashMap()
+                    hashMap.put("userId",userId)
+                    hashMap.put("userName",tempUser)
+                    dbRef.setValue(hashMap).addOnCompleteListener(this){
+                        if(it.isSuccessful){
+                            startChat()
+                        }
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        textInputPassword.setText(null)
+                }
+            }
+    }
+    private fun loginAuth(tempEmail: String, tempPassword: String){
+        if(tempEmail!="" && tempPassword!="") {
+            auth.signInWithEmailAndPassword(tempEmail, tempPassword)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
                         val user = auth.currentUser
                         startChat()
                     } else {
                         // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
                         Toast.makeText(
                             baseContext,
                             "Authentication failed.",
                             Toast.LENGTH_SHORT,
                         ).show()
-                        textViewStatus.text = "Failed to log in. Nonexistent account or wrong password!"
-                        textViewStatus.isVisible = true
-                        textInputPassword.setText("")
+                        textInputPassword.setText(null)
                     }
                 }
         }
     }
-
     private fun startChat() {
         val currentUser = auth.currentUser
         if (currentUser != null) {
-            val intent = Intent(this, ChatActivity::class.java)
-            intent.putExtra("user", currentUser.toString())
-            startActivity(intent)
+            textInputUser.setText(null)
+            textInputEmail.setText(null)
+            textInputPassword.setText(null)
             Toast.makeText(
                 baseContext,
                 "Login successful!",
                 Toast.LENGTH_SHORT,
             ).show()
+            val intent = Intent(this, ChatActivity::class.java)
+            intent.putExtra("user", currentUser.toString())
+            startActivity(intent)
             finish()
-
         }
     }
 }
